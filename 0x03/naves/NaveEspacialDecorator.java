34b28c49d72bcafcdb193e5b5b80b719f6b81030
Eliabe-ciandt <eliabe@ciandt.com>
2022-08-18 16:57:27 -0300

    Decorator Java

---
+++ 0x03/naves/NaveEspacialDecorator.java
@@ -0,0 +1,20 @@
+public class NaveEspacialDecorator extends NaveEspacial {
+
+    private NaveEspacial naveEspacialDecorada;
+
+    public NaveEspacialDecorator(NaveEspacial naveEspacial) {
+        super(naveEspacial.getSaude(), naveEspacial.getAtaque());
+        this.naveEspacialDecorada = naveEspacial;
+    }
+
+    @Override
+    public int getSaude() {
+        return naveEspacialDecorada.getSaude();
+    }
+
+    @Override
+    public int getAtaque() {
+        return naveEspacialDecorada.getAtaque();
+    }
+
+}
