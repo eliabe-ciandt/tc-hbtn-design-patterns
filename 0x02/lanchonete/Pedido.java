5e5046e2a3f114f628b22b07060d8e4fe650328d
Eliabe-ciandt <eliabe@ciandt.com>
2022-08-18 12:54:15 -0300

    Builder Java

--- 0x02/lanchonete/Pedido.java
+++ 0x02/lanchonete/Pedido.java
@@ -15,8 +15,8 @@ public class Pedido {
 
     @Override
     public String toString() {
-        String itensFora = "        Fora da caixa:";
-        String itensDentro = "        Dentro da caixa:";
+        String itensFora = "        Fora da Caixa:";
+        String itensDentro = "        Dentro da Caixa:";
 
         for(ItemPedido itemPedido: itensDentroCaixa) {
             itensDentro += "\n                - " +itemPedido.getTipo() + " " + itemPedido.getNome();
